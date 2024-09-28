9b02fa94e80973ad836af468b30d8ce3950c096e
hunzsig <mzyhaohaoren@qq.com>
2024-09-28 11:29:29 +0800

    feat(assets): *

--- assets/project.lua
+++ assets/project.lua
@@ -4,11 +4,6 @@ assets_font("文泉驿等宽微米黑（WenQuanYi Micro Hei Mono）")
 
 assets_terrain("xlik")
 
-assets_icon("black")
-
-assets_vwp("metal_heavy_chop")
-assets_vwp("wood_heavy_bash")
-
 assets_ui("xlik_info")
 assets_ui("xlik_plate")
 assets_ui("xlik_buff")
